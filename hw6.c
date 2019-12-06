216964dcdaecd796d7936b5d09b3879298e042e6
kevinbarrett11 <kbarre8@uic.edu>
2019-12-06 17:35:27 -0600

    Added more elvators

--- hw6.c
+++ hw6.c
@@ -1,112 +1,126 @@
 #include "hw6.h"
 #include <stdio.h>
 #include<pthread.h>
+#include <time.h>
+#include <stdlib.h>
 
 
+struct Elevator {
+    pthread_mutex_t lock;  //a lock so nothing else takes control of that elevator
+    pthread_barrier_t barrierOpen, barrierClosed;   //stores barriers for each elevator so that the global elevator data isnt overwritten
 
-pthread_mutex_t lock;
-pthread_barrier_t barr;
-
-
-static struct Passenger {
-    int id;
-    int from_floor;
-    int to_floor;
-    int in_elevator;
-    enum {WAITING, ENTERED, EXITED} state;
-} passengers[PASSENGERS];
+    int current_floor;
+    int direction; //direction elevator is going aka up or down
+    int to_floor;  //destination of where elevator is going to pick up or drop off
+    int occupancy;  //how many passengers are in elevator, default being 0
+    enum { ELEVATOR_ARRIVED, ELEVATOR_OPEN, ELEVATOR_CLOSED } state;
+}elevators[ELEVATORS];
 
 
-int current_floor;
-int direction;
-int occupancy;
-enum {ELEVATOR_ARRIVED=1, ELEVATOR_OPEN=2, ELEVATOR_CLOSED=3} state;
-
+//initilizes all the variables when main calls scheduler init for all 4 elevators which is assigned via main.c or test1 ect
 void scheduler_init() {
-    current_floor=0;
-    direction=-1;
-    occupancy=0;
-    state=ELEVATOR_ARRIVED;
-    pthread_mutex_init(&lock,0);
-    pthread_barrier_init(&barr,NULL,2);
+    for (int i = 0; i < ELEVATORS; i++) {  //elevators is assigned via main.c or test which changes between 4 and 1
+        elevators[i].current_floor =  random() % ELEVATORS; //sets all elevators floors to random so that it can prossess passnegers all throughout building faster
+        elevators[i].direction = -1;  //default direction to -1
+        elevators[i].occupancy = 0;  //default elevator empty
+        elevators[i].state = ELEVATOR_ARRIVED;  //default state to arrived
+        pthread_mutex_init(&elevators[i].lock, 0);  //mutex lock to prevent critical data from being changed
+        pthread_barrier_init(&elevators[i].barrierOpen, NULL, 2);  //barrier to pass control to passanger or elevator
+        pthread_barrier_init(&elevators[i].barrierClosed, NULL, 2);  //closes barrier
+        elevators[i].to_floor = 0;  //sets default to floor to zero because they arnt going anywhere yet until passenger request
+    }
 }
 
 
 void passenger_request(int passenger, int from_floor, int to_floor,
-                       void (*enter)(int, int),
+                       void(*enter)(int, int),
                        void(*exit)(int, int))
 {
-        struct Passenger p;
-    passengers[0] = p;
 
-    p.from_floor = from_floor;
-    p.to_floor = to_floor;
-    p.id = passenger;
+    //attempt to get closets elevator to passenger to run
+//    int elevatorDistance[4] = {0 , 0 ,0, 0};
+//
+//    for(int i = 0; i < 3; i++){
+//        if( elevators[i].occupancy== 1){
+//            elevatorDistance[i] = 99;
+//        } else {
+//            if (elevators[i].current_floor > from_floor){
+//              elevatorDistance[i] =  elevators[i].current_floor - from_floor;
+//            } else
+//            elevatorDistance[i] = from_floor - elevators[i].current_floor;
+//        }
+//    }
+//
+//
+//    int closest = 0;
+//    for(int j = 1; j < 3; j++){
+//        if(elevatorDistance[j] < elevatorDistance[closest]){
+//            closest = j;
+//        }
+//    }
+//    for (int l = 0; l < 3; l++){
+//        elevatorDistance[l] = 0;
+//    }
+//    elevator = closest;
+
+    int elevator = random() % ELEVATORS;
+
+    //if elevator full then choose different elevator
+    while (elevators[elevator].occupancy == 1)
+    {
+        elevator = random() % ELEVATORS;
+    }
+
+    pthread_mutex_lock(&elevators[elevator].lock); //locks critical data btween this and unlock
+
+    elevators[elevator].to_floor = from_floor; //tells the elvator what floor it has to go to pick someone up
+
+    //passes control to passneger to allow them to get on
+    pthread_barrier_wait(&elevators[elevator].barrierOpen);
+    if (elevators[elevator].occupancy == 0)
+    {
+        enter(passenger, elevator);
+        elevators[elevator].occupancy++;  //increase
+    }
+    elevators[elevator].to_floor = to_floor;  //changes value to the floor of where passenger is going
+    pthread_barrier_wait(&elevators[elevator].barrierClosed);
 
 
-    // wait for the elevator to arrive at our origin floor, then get in
-pthread_barrier_wait(&barr);
+    //passes control to passenger then lets passenger to exit
+    pthread_barrier_wait(&elevators[elevator].barrierOpen);
+    if (elevators[elevator].occupancy == 1)
+    {
+        exit(passenger, elevator);
+        elevators[elevator].occupancy--;
+    }
+    elevators[elevator].to_floor = -1;
+    pthread_barrier_wait(&elevators[elevator].barrierClosed);  //passes control back to elevator
 
-
-        if(current_floor == from_floor && state == ELEVATOR_OPEN && occupancy==0) {
-            enter(passenger, 0);  //change this to look for next elevator
-            occupancy++;
-            pthread_barrier_wait(&barr);
-
-        }
-
-
-
-    // wait for the elevator at our destination floor, then get out
-pthread_barrier_wait(&barr);
-
-        if(current_floor == to_floor && state == ELEVATOR_OPEN) {
-            exit(passenger, 0);
-            occupancy--;
-            pthread_barrier_wait(&barr);
-        }
-
+    pthread_mutex_unlock(&elevators[elevator].lock);  //unlocks critical data
 }
 
 void elevator_ready(int elevator, int at_floor,
                     void(*move_direction)(int, int),
                     void(*door_open)(int), void(*door_close)(int)) {
-    if(elevator!=0) return;
 
-    current_floor = at_floor;
-    
-    if(current_floor != passengers[0].from_floor && occupancy==0) {
-        current_floor = passengers[0].from_floor;
+    if (elevators[elevator].to_floor == at_floor && elevators[elevator].state == ELEVATOR_ARRIVED) {
+        door_open(elevator);  //opens the door for passenger
+        elevators[elevator].state = ELEVATOR_OPEN;  //sets elevator state to open
+        pthread_barrier_wait(&elevators[elevator].barrierOpen); //gives passenger time to get on
+        pthread_barrier_wait(&elevators[elevator].barrierClosed);  //returns control to elevator
     }
-
-    pthread_mutex_lock(&lock);
-    if(state == ELEVATOR_ARRIVED) {
-        door_open(elevator);
-        state=ELEVATOR_OPEN;
-        pthread_barrier_wait(&barr);
-    }
-    else if(state == ELEVATOR_OPEN) {
-        pthread_barrier_wait(&barr);
-        door_close(elevator);
-        state=ELEVATOR_CLOSED;
+    else if (elevators[elevator].state == ELEVATOR_OPEN) {
+        door_close(elevator);  //if the elevator is open it closes it assuimging that passenger had time to get on
+        elevators[elevator].state = ELEVATOR_CLOSED;  //changes elevator state to closed
     }
     else {
-        if(at_floor==0 || at_floor==FLOORS-1)
-            direction*=-1;
-        move_direction(elevator,direction);
-        current_floor=at_floor+direction;
-        state=ELEVATOR_ARRIVED;
+        if (at_floor == 0 || at_floor == FLOORS - 1)
+            elevators[elevator].direction *= -1;
+        if (elevators[elevator].to_floor != -1) {
+            move_direction(elevator, elevators[elevator].direction);
+            elevators[elevator].current_floor = at_floor + elevators[elevator].direction;
+        }
+        elevators[elevator].state = ELEVATOR_ARRIVED;
+
     }
-    pthread_mutex_unlock(&lock);
 }
-
-
-
-//static struct Elevator {
-//    int seqno, last_action_seqno; // these two are used to enforce control rules
-//    int floor;
-//    int open;
-//    int passengers;
-//    int trips;
-//    int passengerNum;
-//} elevators[ELEVATORS];
